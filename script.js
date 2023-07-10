165da8d11a3fa6f90cad8623ce744734d66cffbd
RyanStokes95 <ryanstokes560@gmail.com>
2023-07-10 23:54:29 +0100

    Added values to number buttons

---
+++ script.js
@@ -0,0 +1,9 @@
+var buttons = document.getElementsByClassName("numberBtn")
+var screen = document.getElementById("inputScreen")
+for (let i = 0; i < buttons.length; i++) {
+    var values = [7,8,9,4,5,6,1,2,3,0]
+    buttons[i].value = values[i]
+    buttons[i].addEventListener("click", print)
+}
+
+console.log(buttons.values)
