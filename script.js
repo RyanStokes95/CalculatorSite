3ccd16c751d0e134553a52ef84aff133aa2dad02
RyanStokes95 <ryanstokes560@gmail.com>
2023-07-13 17:11:40 +0100

    Added Clear button

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 var buttons = document.getElementsByClassName("numberBtn")
 var screen = document.getElementById("inputScreen")
-for (let i = 0; i < buttons.length; i++) {
-    var values = [7,8,9,4,5,6,1,2,3,0]
-    buttons[i].value = values[i]
-    buttons[i].addEventListener("click", print = () => {
-        screen.innerHTML = screen.innerHTML + buttons[i].value;
+var clear = document.getElementById("clear")
+
+function numberSetUp(){
+    for (let i = 0; i < buttons.length; i++) {
+        var values = [7,8,9,4,5,6,1,2,3,0]
+        buttons[i].value = values[i]
+        buttons[i].addEventListener("click", print = () => {
+            screen.innerHTML += buttons[i].value;
+            }
+        )
+    }
+}
+
+numberSetUp();
+
+function clearSetUp(){
+    clear.addEventListener("click", clearFunc = () => {
+        screen.innerHTML = "";
         }
     )
 }
+
+clearSetUp();
