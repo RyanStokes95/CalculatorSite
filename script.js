15e09b92f717c9c959c0d57496f11f64483d561e
RyanStokes95 <ryanstokes560@gmail.com>
2023-07-13 16:57:28 +0100

    Number Buttons set up and correctly printing values to the screen

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ var screen = document.getElementById("inputScreen")
 for (let i = 0; i < buttons.length; i++) {
     var values = [7,8,9,4,5,6,1,2,3,0]
     buttons[i].value = values[i]
-    buttons[i].addEventListener("click", print)
+    buttons[i].addEventListener("click", print = () => {
+        screen.innerHTML = screen.innerHTML + buttons[i].value;
+        }
+    )
 }
-
-console.log(buttons.values)
